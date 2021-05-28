97717562586ef6a63bd5eb7b63b4eff69c142038
Harry Mouat <harry@Harrys-MacBook-Air.local>
2021-05-28 23:01:10 +0100

    README edit@

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # M2R-Julia-Sets
+![The Multibrot set for the function z^3 + b](threebrot.png)
--- draw_threebrot.py
+++ draw_threebrot.py
@@ -1,6 +1,6 @@
 from julia import CubicMap
 
 threebrot = CubicMap(a=0)
-threebrot.draw_multibrot(res_x=400, res_y=400, iterations=100, x_range=(-2, 2), y_range=(-2, 2))
+threebrot.draw_multibrot(res_x=2048, res_y=2048, iterations=100, x_range=(-2, 2), y_range=(-2, 2), z_max=2)
 threebrot.b = complex(1/2)
 threebrot.draw_julia()
